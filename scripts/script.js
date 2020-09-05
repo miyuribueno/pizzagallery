171f08a223c120f86732d8f344a1b466210f1a30
miyuri bueno <buenomiyuri@gmail.com>
2020-09-05 12:13:12 +0800

    feat: fixed contact form, separated js file

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+    ScrollReveal({ reset: true });
+    sr.reveal('#navbar', {
+      duration: 1500,
+      origin: 'left',
+      distance: '50px'
+    });
+
+    sr.reveal('#sign', {
+      duration: 2000,
+      origin: 'right',
+      distance: '500px'
+    });
+
+    sr.reveal('#title2', {
+      duration: 3000,
+      origin: 'right',
+      distance: '500px'
+    });
+
+    sr.reveal('#subhead', {
+      duration: 3700,
+      origin: 'right',
+      distance: '500px'
+    });
+
+    sr.reveal('#carousel', {
+      duration: 2200,
+      origin: 'left',
+      distance: '300px'
+    });
